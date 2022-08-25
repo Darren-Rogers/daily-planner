499a0ee9ddc73ae959161c61270298e404ba3471
Sreggie <darren.rogers06@gmail.com>
2022-08-25 19:47:43 -0400

    added notes describing each function

--- assets/script.js
+++ assets/script.js
@@ -1,4 +1,7 @@
+//Displays the current time and date under the heading
 var currentTime = $('#currentDay').html(moment().format('LT MMMM Do, YYYY'));
+//for loop creates a row with a time, input area and save button for the business hours in a day(9-5)
+//if/else statesments check the current time and color code the input areas to display if the hours are past, present or in the future 
 for (var i = 9; i <= 17; i++) {
     var parentEl = $('<div class="row" data-id="' + i + '">');
     var idEl = $('<div class="col 1">' + i + '</div>');
@@ -16,11 +19,13 @@ for (var i = 9; i <= 17; i++) {
         textEl.addClass('past')
     }
 }
+//when the save button is click the time and activity entered are saved to local storage
 $('.container').on('click', '.save', function () {
     var parentId = $(this).parent().attr('data-id');
     var toDo = $(this).prev().val();
     localStorage.setItem(parentId, toDo);
 })
+//items saved to local storage are retrieved and stay on the page when refreshed
 $('.activity').each(function(){
     var parentId = $(this).parent().attr('data-id');
     var getToDo = localStorage.getItem(parentId);
